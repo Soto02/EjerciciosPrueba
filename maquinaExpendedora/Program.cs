d6d8dc2f33c6ed58d9c677840ca0d72d5637bb2e
Soto02 <asotfer636@g.educaand.es>
2023-09-25 20:39:18 +0200

    maquinaExpendedora
    calcula el menor numero de monedas necesarias para devolver el cambio en una maquina expendedora

---
+++ maquinaExpendedora/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace monedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double producto = 0.45, cambio, centimos;
+            double dinero = 0, contador, euros;
+
+
+            Console.WriteLine("Todos los productos cuestan 0.45 centimos");
+            Console.WriteLine("Introduzca el dinero (monedas): ");
+            dinero = Convert.ToDouble(Console.ReadLine());
+            cambio = dinero - producto;
+            contador = cambio;
+            
+            for (double i = 0; i < contador; i++)
+            {
+                
+            }
+
+
+            
+
+
+            Console.WriteLine("Su cambio es de: " + cambio);
+            
+
+            
+
+            
+
+
+        }
+    }
+}
