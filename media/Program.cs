008eeebe8adcbbb333c6b54f6fd9f3ec80c614a0
Soto02 <asotfer636@g.educaand.es>
2023-09-25 19:53:08 +0200

    media
    pide 4 numeros y calcula la media de los 4

---
+++ media/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace media
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, numero3, numero4;
+
+            Console.WriteLine("Introduzca el primer número: ");
+             numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo número: ");
+             numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el tercer número: ");
+             numero3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el cuarto número: ");
+             numero4 = int.Parse(Console.ReadLine());
+
+            double media = Convert.ToDouble(numero1 + numero2 + numero3 + numero4) / 4;
+            Console.WriteLine("La media de los 4 números es: " + media);
+
+
+        }
+    }
+}
