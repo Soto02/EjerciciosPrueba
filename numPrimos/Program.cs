c2048813d9ad7bcee50185660e305aa00fa01bda
Soto02 <asotfer636@g.educaand.es>
2023-09-25 19:20:47 +0200

    numPrimos
    Aplicacion que muestra los 5 primeros numeros primos

---
+++ numPrimos/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace numerosPrimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2;
+            int divisible = 0;
+
+            while (num <= 12)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        divisible++;
+                    }
+                        
+                    if (divisible > 2)
+                    {
+                        break;
+                    }
+                        
+                }
+                if (divisible == 2)
+                {
+                    Console.WriteLine(num);
+                }
+                divisible = 0;
+                num++;
+            }
+        }
+    }
+}
