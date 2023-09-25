279acffcb14cb34b8ccc50fa72fa1134180d6f6d
Soto02 <asotfer636@g.educaand.es>
2023-09-25 19:40:33 +0200

    entradaPantalla
    ejercicio que pide entero y lo muestra por pantalla

---
+++ entradapantalla/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace entradaPantalla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine("Introduzca un número entero: ");
+            String dato = Console.ReadLine(); 
+            
+            numero = Convert.ToInt32(dato);
+
+            Console.WriteLine("Has introducido el siguiente número: {0}", numero);
+        }
+    }
+}
