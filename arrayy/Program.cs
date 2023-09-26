6bf1de481a6e11f26e975bf91dcc5e28d9ca6b28
Soto02 <asotfer636@g.educaand.es>
2023-09-26 12:31:01 +0200

    array
    como funciona los array

---
+++ arrayy/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Declaramos el array
+int[] array;
+
+//Inicializamos, si colocamos {1,2,3} lo estamos inicializando con valores
+array = new int[] {1,2,3,4,5};
+
+/*
+//Inicializamos con dos dimensiones
+int[,] matriz = new int[3, 4];
+
+//para acceder
+matriz[0,0] = 0;
+
+//para recorrerlo
+for (int i = 0; i < array.Length; i++)
+{
+    //for(int j = 0; j < matriz.Length; j++)
+}
+*/
+
+var ultimoNum = array[^1];
+
+
+
