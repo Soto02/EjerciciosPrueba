2b8a9a1a443ffb82912bf3f12dff7819e422f708
Soto02 <asotfer636@g.educaand.es>
2023-11-06 12:59:24 +0100

    Piscina Java

---
+++ 1/Piscina/src/main/java/com/mycompany/piscina/DebajoDeCero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.piscina;
+
+/**
+ *
+ * @author dam
+ */
+public class DebajoDeCero extends Exception{
+    public DebajoDeCero(String mensaje) {
+        super(mensaje);
+    }
+    
+}
